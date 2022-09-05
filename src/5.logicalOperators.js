cb22476ec89c2047342dbd40309be536f6d23f8b
palcicjm <palcicjm@mail.uc.edu>
2022-09-05 01:26:46 -0400

    fixed logical op

--- src/5.logicalOperators.js
+++ src/5.logicalOperators.js
@@ -1,25 +1,23 @@
 // use the logical operator not if statements
-
+falseVariable = false
 // or: should return true if either a or b are truthy. False, if both are false
-
-a = truthy;
-b = truthy;
-
 function or(a, b) {
-  expect(logicalOperatorsAnswers.or(false, true)).toBeTruthy();
-  expect(logicalOperatorsAnswers.or(true, false)).toBeTruthy();
-  expect(logicalOperatorsAnswers.or(true, true)).toBeTruthy();
-  expect(logicalOperatorsAnswers.or(false, false)).toBeFalsy();
+  if (a == truthy || b == truthy) {
+    trueVariable = true;
+    return trueVariable;
   }
+  else {return falseVariable;}
+}
 
 //and: should return true only if both a and b are truthy
-
 function and(a, b) {
-  expect(logicalOperatorsAnswers.and(false, true)).toBeFalsy();
-  expect(logicalOperatorsAnswers.and(false, false)).toBeFalsy();
-  expect(logicalOperatorsAnswers.and(true, false)).toBeFalsy();
-  expect(logicalOperatorsAnswers.and(true, true)).toBeTruthy();
+  if (a && b == truthy) {
+    trueVariable = true;
+    
+    return true;
+  }
   
+  else {return falseVariable}
 }
 
 module.exports = {
