f1236e312f2864a395ed665b67f929061bec5438
palcicjm <palcicjm@mail.uc.edu>
2022-09-04 23:45:40 -0400

    actually fixed arrays

--- src/3.arrays.js
+++ src/3.arrays.js
@@ -8,31 +8,45 @@
 arr = [2,4,5,6]
 item = 2;
 function indexOf(arr, item) {
-  return ;
+  arr = [2,4,5,6]
+  index = indexOf(arr, 2);
+  return index;
 }
 
 // Sum: you should be able to sum the items of an array
 // Example: arr = [1,2,3]   =>  6
 function sum(arr) {
-  return ;
+const arr = [2,4,5,6];
+
+const sum = arr.reduce((accumulator, value) => {
+  return accumulator + value;
+}, 0);
+
+  return sum;
 }
 
 // Filter: Should filter out all instances of a value from an array
 // Example: filtering 2 out of [1,2,3,5,6,2,4,2] should return [1,3,5,6,4]
 function filterOut(arr, item) {
-  return ;
+  arr = [2,4,5,6];
+
+const result = arr.filter(2);
+  return result;
 }
 
 // Append: you should be able to add an item to the end of an array
 function append(arr, item) {
-
-  return ;
+  arr = [2,4,5,6];
+  appended = arr.push(10);
+  return appended;
 }
 
 // Truncate: you should be able to remove the last item of an array
 // hint: pop, slice, splice would all work. Pay attention to the return value in the docs
 function truncate(arr) {
-  return ;
+  arr = [2,4,5,6];
+  arrDone = arr1.splice(3);
+  return arrDone;
 }
 
 arr1 = [4,5,1,6]
@@ -42,7 +56,8 @@ arr2 = [3,5,7,8]
 function concat(arr1, arr2) {
   arr1 = [4,5,1,6]
   arr2 = [3,5,7,8]
-  return concat(arr1, arr2);
+  arr3 = concat(arr1, arr2);
+  return arr3;
 }
 
 
@@ -50,9 +65,8 @@ function concat(arr1, arr2) {
 // hint: use splice and pay attention to the return value
 function insert(arr, item, index) {
   arr1 = [4,5,1,6]
-  arr1.splice(7,0,3);
-  
-  return arr1.splice(7,0,3);
+  arr2 = arr1.splice(4,0,3)
+  return arr2;
 }
 
 // Square: you should be able to square each number in an array
